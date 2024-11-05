66cfcca91f5658972066f2195c63ef3387fd3e17
Udigithub <142435114+Chamodakavi@users.noreply.github.com>
2024-11-05 20:00:48 +0530

    filter button layout added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,31 +4,59 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../Context/Context';
-
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const {cart,setCart} = useContext(CartContext)
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
-      <Container>
-        <Navbar.Brand>SHopping Mall</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link className='mx-4'><Link style={{textDecoration:'none'}} to='/'>Home</Link></Nav.Link>
-            <Nav.Link className='mx-4'>
-              <Link style={{textDecoration:'none'}} to='/cart'>
-                Cart ({cart.length})
-              </Link>
-            </Nav.Link>
-            <button className='mx-4'>Filter</button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+   <>
+      <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+        <Container>
+          <Navbar.Brand>SHopping Mall</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className='mx-4'><Link style={{textDecoration:'none'}} to='/'>Home</Link></Nav.Link>
+              <Nav.Link className='mx-4'>
+                <Link style={{textDecoration:'none'}} to='/cart'>
+                  Cart ({cart.length})
+                </Link>
+              </Nav.Link>
+              <Button 
+                className='mx-4'
+                onClick={ handleShow }
+              >
+                Filter
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Filter
+          </Button>
+        </Modal.Footer>
+      </Modal>
+   </>
+    
   )
 }
 
